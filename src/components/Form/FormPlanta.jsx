aea9c09dc98113d4067db6f8fc2047d11911dbe5
German Geredus <germangere@gmail.com>
2023-10-31 21:42:29 -0300

    merge logicaPlanta

--- src/components/Form/FormPlanta.jsx
+++ src/components/Form/FormPlanta.jsx
@@ -6,93 +6,93 @@ import { rutas } from "../../consts/consts"
 
 const FormPlanta = () => {
 
-    const defaultValue = {
-        nombre: '',
-        esSemilla: 0,
-        especie: '',
-        genero: '',
-        // cantidad: '',
-        precio: '',
-        // imagen: ''
-    }
+  const defaultValue = {
+    nombre: '',
+    esSemilla: 0,
+    especie: '',
+    genero: '',
+    // cantidad: '',
+    precio: '',
+    // imagen: ''
+  }
 
-    const [value, setValue] = useState(defaultValue)
+  const [value, setValue] = useState(defaultValue)
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setValue((prev) => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setValue((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
 
-    const handleCheckChange = (e) => {
-        setValue((prev) => ({
-            ...prev,
-            esSemilla : e.target.checked ? 1 : 0
-        }))
-    }
+  const handleCheckChange = (e) => {
+    setValue((prev) => ({
+      ...prev,
+      esSemilla: e.target.checked ? 1 : 0
+    }))
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setProducto(value, rutas.productos)
-            .then((res) => {
-                swalOk()
-                setValue(defaultValue)
-                console.log(res)
-            })
-            .catch(error => swalError(error))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setProducto(value, rutas.productos)
+      .then((res) => {
+        swalOk()
+        setValue(defaultValue)
+        console.log(res)
+      })
+      .catch(error => swalError(error))
+  }
 
-    return (
+  return (
 
-        <form id='formPlanta'>
-            <fieldset id="contenedorCargaPlanta">
+    <form id='formPlanta'>
+      <fieldset id="contenedorCargaPlanta">
 
-                <h2>Carga de Datos para Plantas</h2>
+        <h2>Carga de Datos para Plantas</h2>
 
-                <div id="contenedorInputsPlanta">
+        <div id="contenedorInputsPlanta">
 
-                    <div id="categoria">
-                        <input type="text" name="nombre" placeholder="Nombre" onChange={handleChange} value={value.nombre}/>
-                    </div>
+          <div id="categoria">
+            <input type="text" name="nombre" placeholder="Nombre" onChange={handleChange} value={value.nombre} />
+          </div>
 
-                    <div id="categoriaSemilla">
-                        <label>Semilla</label>
-                        <input type="checkbox" name="esSemilla" onChange={handleCheckChange} value={value.esSemilla}/>
-                    </div>
+          <div id="categoriaSemilla">
+            <label>Semilla</label>
+            <input type="checkbox" name="esSemilla" onChange={handleCheckChange} value={value.esSemilla} />
+          </div>
 
 
-                    <div id="categoria">
-                        <input type="text" name="especie" placeholder="Especie" onChange={handleChange} value={value.especie}/>
-                    </div>
+          <div id="categoria">
+            <input type="text" name="especie" placeholder="Especie" onChange={handleChange} value={value.especie} />
+          </div>
 
-                    <div id="categoria">
-                        <input type="text" name="genero" placeholder="Genero" onChange={handleChange} value={value.genero}/>
-                    </div>
+          <div id="categoria">
+            <input type="text" name="genero" placeholder="Genero" onChange={handleChange} value={value.genero} />
+          </div>
 
-                    {/* <div id="categoria">
+          {/* <div id="categoria">
                         <input type="number" name="cantidad" placeholder="Cantidad" onChange={handleChange} value={value.cantidad}/>
                     </div> */}
 
-                    <div id="categoria">
-                        <input type="number" name="precio" placeholder="Precio"  onChange={handleChange} value={value.precio}/>
-                    </div>
+          <div id="categoria">
+            <input type="number" name="precio" placeholder="Precio" onChange={handleChange} value={value.precio} />
+          </div>
 
-                    {/* <div id="categoriaImagen">
+          {/* <div id="categoriaImagen">
                         <label htmlFor="imagen">Presione AQUI para cargar una imagen</label>
                         <input id="imagen" name="imagen" type="file" display="none" onChange={handleChange} value={value.imagen}/>
                     </div> */}
 
-                    <button onClick={handleSubmit}>CARGAR</button>
+          <button onClick={handleSubmit}>CARGAR</button>
 
-                </div>
+        </div>
 
-            </fieldset>
+      </fieldset>
 
-        </form>
+    </form>
 
-    )
+  )
 
 
 }
